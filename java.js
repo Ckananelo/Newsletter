cb46ad8c0faf58449570eed8535e430c5006f46b
christopher mokoena <ckananelo4@gmail.com>
2023-11-26 16:06:01 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,16 @@
+var Email = document.getElementById("Input").innerHTML
+
+function Subscribe(){
+
+    document.getElementById("Thanks").style.display = "block"
+    document.getElementById("main").style.display = "none"
+
+}
+
+function Dismiss(){
+
+    document.getElementById("Thanks").style.display = "none"
+    document.getElementById("main").style.display = "flex"
+    
+}
+
